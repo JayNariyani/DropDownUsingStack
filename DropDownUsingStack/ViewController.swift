0fd220d9d4cceebd1cbf8aae2e63d1badb25e4fd
Jay Nariyani <nariyanijay@gmail.com>
2020-07-01 20:21:13 +0530

    Revert "Initial Commit"
    This reverts commit ae9ea7afa93b46f439196504414e7703208c07c7.

--- DropDownUsingStack/ViewController.swift
+++ DropDownUsingStack/ViewController.swift
@@ -9,31 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-    
-    @IBOutlet var cities: [UIButton]!
-    
 
-    @IBAction func selectcity(_ sender: UIButton) {
-        cities.forEach { (Button) in
-            UIView.animate(withDuration: 0.3, animations: {
-                Button.isHidden = !Button.isHidden
-                self.view.layoutIfNeeded()
-            })
-        }
-    }
-    
-    @IBAction func city1(_ sender: UIButton) {
-    }
-    
-    @IBAction func city2(_ sender: UIButton) {
-    }
-    
-    @IBAction func city3(_ sender: UIButton) {
-    }
-    
-    @IBAction func city4(_ sender: UIButton) {
-    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
